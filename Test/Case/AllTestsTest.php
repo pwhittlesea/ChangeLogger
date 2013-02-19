7f23c778e62db71371e08b196934061794afe120
Phillip Whittlesea <pw.github@thega.me.uk>
2013-02-19 19:36:47 +0000

    Adding test case to recessively run all tests

---
+++ Test/Case/AllTestsTest.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Custom test suite to execute all tests
+ */
+
+class AllTestsTest extends PHPUnit_Framework_TestSuite {
+
+    public static function suite() {
+
+        $path = App::pluginPath('ChangeLogger') . 'Test' . DS . 'Case' . DS;
+
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectoryRecursive($path . 'Model' . DS);
+        return $suite;
+
+    }
+
+}
